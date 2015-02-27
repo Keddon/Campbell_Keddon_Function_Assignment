75c1d17c738fe79b0058bad7ccf1ec89173ba505
Keddon Campbell <mrkfed101@gmail.com>
2015-02-26 21:01:05 -0500

    Assignment
    Adding code to prevent an infinite loop

--- js/script.js
+++ js/script.js
@@ -3,17 +3,25 @@
  */
 
 var userChoice = prompt("What do you prefer? \n florida lottery or powerball? \n Type one of the two choices below");
+var retryTimes = 1;
+
 
 while (userChoice === ("") || (!isNaN(userChoice))){
     getPreference();
+    retryTimes = retryTimes ++;
+    console.log(retryTimes);
 }
-if (userChoice !== ("poweball") || ("florida lottery")){
+while (userChoice !== ("powerball") || ("florida lottery")){
     getPreference();
+    retryTimes = retryTimes ++;
+    console.log(retryTimes)
 }
 
-function getPreference(){
-    userChoice = prompt("Sorry we didn't get that. Try typing \n florida lottery of powerball");
-    console.log("retry!");
-    
+function getPreference(retryTimes){
+    while (retryTimes <= 20) {
+        userChoice = prompt("Sorry we didn't get that. Try typing \n florida lottery of powerball");
+        console.log("retry!");
+    }
 }
-console.log("here ");
+console.log(userChoice);
+console.log(retryTimes);
