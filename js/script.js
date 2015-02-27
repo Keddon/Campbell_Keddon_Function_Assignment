e91da8497f1d16b290b4823ea36746cebb977e53
Keddon Campbell <mrkfed101@gmail.com>
2015-02-26 20:45:43 -0500

    Assignment
    Added the function which prompts for user input

--- js/script.js
+++ js/script.js
@@ -2,3 +2,18 @@
  * Created by Keddon Campbell on 2/26/15.
  */
 
+var userChoice = prompt("What do you prefer? \n florida lottery or powerball? \n Type one of the two choices below");
+
+while (userChoice === ("") || (!isNaN(userChoice))){
+    getPreference();
+}
+if (userChoice !== ("poweball") || ("florida lottery")){
+    getPreference();
+}
+
+function getPreference(){
+    userChoice = prompt("Sorry we didn't get that. Try typing \n florida lottery of powerball");
+    console.log("retry!");
+    
+}
+console.log("here ");
