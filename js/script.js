f634151db600353b6f48d078798eb2f27e2a74f9
Keddon Campbell <mrkfed101@gmail.com>
2015-02-26 21:39:04 -0500

    testing
    Added more code to prevent an infinite loop

--- js/script.js
+++ js/script.js
@@ -4,24 +4,29 @@
 
 var userChoice = prompt("What do you prefer? \n florida lottery or powerball? \n Type one of the two choices below");
 var retryTimes = 1;
+var retryCount = retryTimes;
 var userInput = userChoice;
 
 
 
 
 
-while (userInput !== ("powerball") || ("florida lottery")){
-    getPreference();
-    userInput = userChoice;
-    retryTimes = retryTimes ++;
-    console.log(retryTimes + " level 2");
+while (userInput != ("powerball") || ("florida lottery")){
+    if (retryCount <= 10) {
+        getPreference(userChoice);
+        userInput = userChoice;
+        retryTimes = retryCount++;
+        console.log(retryCount + " This is hard work");
+    }
 }
 
-function getPreference() {
-    userChoice = prompt("Sorry we didn't get that. Try typing \n florida lottery or powerball");
-    console.log("retried!");
-    return userChoice;
-
+function getPreference(userChoice) {
+        userChoice = prompt("Sorry we didn't get that. Try typing \n florida lottery or powerball");
+        console.log("retried!");
+        userInput = userChoice;
+        console.log(userChoice);
+        console.log(userInput + " is what I have....");
+        return userChoice;
 }
 console.log(userChoice);
 console.log(retryTimes);
